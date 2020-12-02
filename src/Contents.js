162cab272329923052ccffd1a79ead78b5c45170
Dylan Nguyen <nguye2dd@dukes.jmu.edu>
2020-12-01 22:54:46 -0500

    styling

--- src/Contents.js
+++ src/Contents.js
@@ -22,10 +22,11 @@ export function Contents(props) {
   const recipes = props.recipes;
   console.log(recipes);
   return (
-    <div>
+    <div id="root">
       <h1>Table of Contents</h1>
+      <hr></hr>
       <ul>
-        {recipes.map(recipe => <li key= {recipe.id}> {recipe.name} </li>)}
+        {recipes.map(recipe => <li class="content" key= {recipe.id}> {recipe.name} </li>)}
       </ul>
     </div>
   );
